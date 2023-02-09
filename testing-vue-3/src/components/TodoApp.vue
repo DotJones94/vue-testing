0dd9965da14cddfc9ec71f3add79d7a9de4c5c07
KellyJonesSubR <78434731+KellyJonesSubR@users.noreply.github.com>
2023-02-09 12:08:39 +0000

    First simple test

---
+++ testing-vue-3/src/components/TodoApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          text: 'Learn vue testing',
+          completed: false,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ testing-vue-3/tests/unit/TodoApp.spec.js
@@ -0,0 +1,10 @@
+import { mount} from "@vue/test-utils";
+import TodoApp from "@/components/TodoApp";
+
+describe("TodoApp.vue", () => {
+    it('should render todo text', () => {
+        const wrapper = mount(TodoApp);
+
+
+    })
+})
